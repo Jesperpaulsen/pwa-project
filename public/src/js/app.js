2a8dd1360f0fae563bf991291849fa5a9c71071a
Jesper Paulsen <jesper@jesper.no>
2021-06-06 17:07:36 +0200

    add basic cache for offline use

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,8 @@
 
+if (!window.Promise) {
+  window.Promise = Promise
+}
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').then(() => {
     console.log('Service Worker registered!')
